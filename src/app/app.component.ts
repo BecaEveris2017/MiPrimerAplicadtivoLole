8fa36dcdada665576371e7b54b2ef8969af82272
lole <furci_070495@hotmail.com>
2017-10-28 00:30:32 -0500

    tarea2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,22 @@ export class AppComponent {
   title = 'app';
   name = 'Jose Dolores Resendiz'
   colores = ['negro', 'rojo', 'azul']
-
+  mostrar = false;
+  status = 'Mostrar';
+  
   AddColor(Color) {
     this.colores.push(Color.value);
     Color.value = '';
     return false;
+  }
+  DesplegarArray() {
+    this.mostrar = !this.mostrar;
+    if (this.status == 'Ocultar') {
+      this.status = 'Mostrar';
+    }
+    else {
+      this.status = 'Ocultar';
+    }
+
   }
 }
